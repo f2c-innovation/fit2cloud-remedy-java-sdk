2e88fcd49ed751fb2f2847616ce0f4e5c44403ba
maguohao2018 <guohao.ma@fit2cloud.com>
2018-11-26 17:27:41 +0800

    sdk修改11-26

--- src/main/java/com/fit2cloud/selfService/sdk/UBbaClient.java
+++ src/main/java/com/fit2cloud/selfService/sdk/UBbaClient.java
@@ -29,6 +29,13 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
 //import java.util.ArrayList;
 //import java.util.List;
 //
@@ -116,6 +123,8 @@ public class UBbaClient {
     public JSONObject createRemedyCMDBEntry(JSONObject jsonObject, String X_AUTH_APIKEY, String url) {
 
         try {
+            //采用绕过验证的方式处理https请求
+            SSLContext sslcontext = createIgnoreVerifySSL();
 
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
@@ -141,6 +150,8 @@ public class UBbaClient {
     public JSONObject createRemedyChangeEntry(JSONObject jsonObject, String X_AUTH_APIKEY, String url) {
 
         try {
+            //采用绕过验证的方式处理https请求
+            SSLContext sslcontext = createIgnoreVerifySSL();
 
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
@@ -173,6 +184,9 @@ public class UBbaClient {
 
         try {
 
+            //采用绕过验证的方式处理https请求
+            SSLContext sslcontext = createIgnoreVerifySSL();
+
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
             headers.add("x_auth_apikey",X_AUTH_APIKEY);
@@ -197,6 +211,33 @@ public class UBbaClient {
             return null;
 
         }
+    }
+
+    public static SSLContext createIgnoreVerifySSL() throws NoSuchAlgorithmException, KeyManagementException {
+        SSLContext sc = SSLContext.getInstance("SSLv3");
+
+        // 实现一个X509TrustManager接口，用于绕过验证，不用修改里面的方法
+        X509TrustManager trustManager = new X509TrustManager() {
+//            @Override
+            public void checkClientTrusted(
+                    java.security.cert.X509Certificate[] paramArrayOfX509Certificate,
+                    String paramString) throws CertificateException {
+            }
+
+//            @Override
+            public void checkServerTrusted(
+                    java.security.cert.X509Certificate[] paramArrayOfX509Certificate,
+                    String paramString) throws CertificateException {
+            }
+
+//            @Override
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+        };
+
+        sc.init(null, new TrustManager[] { trustManager }, null);
+        return sc;
     }
 
 }
